283aa9a9e94b75bd307ebfca26a1914703a43d0b
devbymanan <devbymanan@gmail.com>
2026-07-06 16:16:43 +0500

    Refactor database configuration and enhance employee management features
    - Removed unused ZK database configuration from .env file.
    - Updated SQL script to create a new RawTransactions table and associated triggers for data synchronization.
    - Modified app.py to remove ZK database connection logic and added functionality to update employee shifts.
    - Created a new add_employee.html template for adding employee details.
    - Enhanced dashboard.html to display total employee count dynamically.
    - Improved employees.html to include search functionality and better employee data presentation.
    - Added modal for assigning shifts to employees in employees.html.
    - Updated shifts.html to display shift details dynamically.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE AttendanceManagementSystem;
 GO
 
-USE AttendanceManagementSystem;
+USE AttendanceManagementSystem
 GO
 
 
@@ -65,3 +65,151 @@ CREATE TABLE SyncLog (
 );
 
 INSERT INTO SyncLog VALUES (0);
+
+
+CREATE TABLE dbo.RawTransactions
+(
+    id              INT NOT NULL PRIMARY KEY,
+
+    emp_code        NVARCHAR(40) NOT NULL,
+
+    punch_time      DATETIME2(7) NOT NULL,
+
+    punch_state     NVARCHAR(10) NOT NULL,
+
+    verify_type     INT NOT NULL,
+
+    work_code       NVARCHAR(40) NULL,
+
+    terminal_sn     NVARCHAR(100) NULL,
+
+    terminal_alias  NVARCHAR(100) NULL,
+
+    area_alias      NVARCHAR(200) NULL,
+
+    longitude       FLOAT NULL,
+
+    latitude        FLOAT NULL,
+
+    gps_location    NVARCHAR(MAX) NULL,
+
+    mobile          NVARCHAR(100) NULL,
+
+    source          SMALLINT NULL,
+
+    purpose         SMALLINT NULL,
+
+    crc             NVARCHAR(200) NULL,
+
+    is_attendance   SMALLINT NULL,
+
+    reserved        NVARCHAR(200) NULL,
+
+    upload_time     DATETIME2(7) NULL,
+
+    sync_status     SMALLINT NULL,
+
+    sync_time       DATETIME2(7) NULL,
+
+    is_mask         SMALLINT NULL,
+
+    temperature     NUMERIC(4,1) NULL,
+
+    emp_id          INT NULL,
+
+    terminal_id     INT NULL,
+
+    company_code    NVARCHAR(100) NULL
+);
+GO
+
+CREATE UNIQUE INDEX UX_RawTransactions
+ON dbo.RawTransactions
+(
+    company_code,
+    emp_code,
+    punch_time
+);
+
+CREATE INDEX IX_RawTransactions_EmpID
+ON dbo.RawTransactions(emp_id);
+
+CREATE INDEX IX_RawTransactions_TerminalID
+ON dbo.RawTransactions(terminal_id);
+GO
+
+
+INSERT INTO dbo.RawTransactions
+SELECT *
+FROM zkbiotime.dbo.iclock_transaction;
+
+CREATE TRIGGER trg_iclock_transaction_insert
+ON zkbiotime.dbo.iclock_transaction
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO dbo.RawTransactions
+    (
+        id,
+        emp_code,
+        punch_time,
+        punch_state,
+        verify_type,
+        work_code,
+        terminal_sn,
+        terminal_alias,
+        area_alias,
+        longitude,
+        latitude,
+        gps_location,
+        mobile,
+        source,
+        purpose,
+        crc,
+        is_attendance,
+        reserved,
+        upload_time,
+        sync_status,
+        sync_time,
+        is_mask,
+        temperature,
+        emp_id,
+        terminal_id,
+        company_code
+    )
+    SELECT
+        id,
+        emp_code,
+        punch_time,
+        punch_state,
+        verify_type,
+        work_code,
+        terminal_sn,
+        terminal_alias,
+        area_alias,
+        longitude,
+        latitude,
+        gps_location,
+        mobile,
+        source,
+        purpose,
+        crc,
+        is_attendance,
+        reserved,
+        upload_time,
+        sync_status,
+        sync_time,
+        is_mask,
+        temperature,
+        emp_id,
+        terminal_id,
+        company_code
+    FROM inserted;
+END;
+GO
+
+
+
+
--- app.py
+++ app.py
@@ -15,13 +15,6 @@ AMS_DB_USER = os.getenv("AMS_DB_USER")
 AMS_DB_PASSWORD = os.getenv("AMS_DB_PASSWORD")
 
 
-ZK_DB_DRIVER = os.getenv("ZK_DB_DRIVER")
-ZK_DB_SERVER = os.getenv("ZK_DB_SERVER")
-ZK_DB_NAME = os.getenv("ZK_DB_NAME")
-ZK_DB_USER = os.getenv("ZK_DB_USER")
-ZK_DB_PASSWORD = os.getenv("ZK_DB_PASSWORD")
-
-
 def AMS_connection():
     conn = pyodbc.connect(
         f"DRIVER={AMS_DB_DRIVER};"
@@ -32,40 +25,122 @@ def AMS_connection():
     )
     return conn
 
-def ZK_connection():
-    conn = pyodbc.connect(
-        f"DRIVER={ZK_DB_DRIVER};"
-        f"SERVER={ZK_DB_SERVER};"
-        f"DATABASE={ZK_DB_NAME};"
-        f"UID={ZK_DB_USER};"
-        f"PWD={ZK_DB_PASSWORD}"
-    )
-    return conn
-
-
-
-
 
 
 @app.route('/')
 def dashboard():
-    return render_template('dashboard.html')
+    conn = None
+    total_employees = 0
+    try:
+        conn = AMS_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT COUNT(*) FROM personnel_employee")
+        total_employees = cursor.fetchone()[0]
+        print(total_employees)
+    except Exception as e:
+        print(f"Error fetching total employees: {e}")
+    finally:
+        if conn:
+            conn.close()
+    return render_template('dashboard.html',total_employees=total_employees)
 
 
 @app.route('/employees')
 def employees():
-    return render_template('employees.html')
+    conn=None
+    try:
+        conn = AMS_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+                        SELECT
+                            p.id AS EmployeeID,
+
+                            CONCAT(
+                                p.first_name,
+                                CASE
+                                    WHEN p.last_name IS NULL OR p.last_name = ''
+                                    THEN ''
+                                    ELSE ' ' + p.last_name
+                                END
+                            ) AS FullName,
+
+                            p.email AS Email,
+
+                            p.emp_code AS ZKBioEmployeeID,
+
+                            e.ShiftID,
+
+                            s.ShiftName
+
+                        FROM personnel_employee p
+
+                        LEFT JOIN EmployeeShiftAssignments e
+                            ON p.id = e.EmployeeID
+
+                        LEFT JOIN Shift s
+                            ON s.ShiftID = e.ShiftID
+
+                        ORDER BY p.id;
+                        """)
+        employees = cursor.fetchall()
+        print(employees)
+    except Exception as e:
+        print(f"Error fetching employees: {e}")
+        employees = []
+    finally:
+        if conn:
+            conn.close()
+    return render_template('employees.html', employees=employees)
 
 @app.route('/add_employee')
 def add_employee():
-    return render_template('employees.html')
+    return render_template('add_employee.html')
+
+@app.route('/update_employee_shift', methods=['POST'])
+def update_employee_shift():
+    emp_id = request.form.get('emp_id')
+    new_shift = request.form.get('new_shift')
+
+    if emp_id and new_shift:
+        conn = None
+        try:
+            conn = AMS_connection()
+            cursor = conn.cursor()
+            cursor.execute("UPDATE personnel_employee SET shift = ? WHERE id = ?", (new_shift, emp_id))
+            conn.commit()
+        except Exception as e:
+            print(f"Error updating employee shift: {e}")
+        finally:
+            if conn:
+                conn.close()
+
+    return redirect(url_for('employees'))
 
 @app.route('/shifts')
 def shifts():
     return render_template('shifts.html')
 
-@app.route('/add_shift')
+@app.route('/add_shift', methods=['GET', 'POST'])
 def add_shift():
+    conn=None
+    try:
+        conn = AMS_connection()
+        cursor = conn.cursor()
+        shift_name = request.form.get('shift_name')
+        start_time = request.form.get('start_time')
+        end_time = request.form.get('end_time')
+        grace_period = request.form.get('grace_period')
+        working_hours = request.form.get('working_hours')
+        cursor.execute("INSERT INTO shift (ShiftName, StartTime, EndTime, GracePeriod, WorkingHours) VALUES (?, ?, ?, ?, ?)", (shift_name, start_time, end_time, grace_period, working_hours))
+        conn.commit()
+        shifts = cursor.fetchall()
+        print(shifts)
+    except Exception as e:
+        print(f"Error adding shift: {e}")
+        shifts = []
+    finally:
+        if conn:
+            conn.close()
     return render_template('shifts.html')
 
 
